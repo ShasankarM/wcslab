b04f205c19f3aedd6d4cf276a9b2699c59095283
sree9398 <121845000+sree9398@users.noreply.github.com>
2023-01-02 20:50:18 -0800

    Add files via upload

---
+++ week 10.1.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    std::cout << "Enter N value:" << std::endl;
+    cin>>n;
+    int arr[n];
+    arr[0]=0;
+    for(int i=1;i<=n;i++)
+    {
+        arr[i]=arr[i/2]+i%2;
+    }
+    for(int i=1;i<=n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+}   
